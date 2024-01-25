c4366f5b324ddbb80fe565ffc72f72346107317e
Vruti Soni <vrutisni@gmail.com>
2024-01-25 15:15:51 -0500

    adding the changes

--- js/script.js
+++ js/script.js
@@ -1,21 +1,88 @@
-console.log("Hello world!");
+console.log("Hello World!")
 
-const myName = "Vruti Soni";
-const h1 = document.querySelector(".heading-primary");
-console.log(myName);
-console.log(h1);
+const myName = "Vruti Soni"
+const h1 = document.querySelector(".heading-primary")
+console.log(myName)
+console.log(h1)
 
-// h1.addEventListener("click", function () {
+// h1.addEventListener("click", function(){
 //   h1.textContent = myName;
-//   h1.style.backgroundColor = "red";
-//   h1.style.padding = "5rem";
-// });
+//   h1.style.backgroundColor ="red";
+//   h1.style.padding = "5rem"
+// })
 
 ///////////////////////////////////////////////////////////
 // Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
-yearEl.textContent = currentYear;
+yearEl.textContent = currentYear
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work 
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation 
+
+const allLinks = document.querySelectorAll('a:link');
+console.log(allLinks);
+allLinks.forEach(function(link){
+  link.addEventListener('click', function(e){
+    // console.log(e) //gives the event for e 
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //scroll back to the top 
+    if (href === "#")window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+
+    })
+
+    //scroll to other links
+    if (href !== "#" && href.startsWith("#")){
+        const sectionEl = document.querySelector(href)
+        sectionEl.scrollIntoView({ behavior: "smooth"})
+
+    }
+
+    //close mobile navigation 
+    if(link.classList.contains('main-nav-link')){
+      headerEl.classList.toggle('nav-open')
+    }
+
+  });
+})
+///////////////////////////////////////////////////////////
+// Sticky nagivation 
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
